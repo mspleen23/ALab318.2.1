a6efd1e79fc46b360ca76dbcc2ca49dcf96bb9c8
Melissa <mespleen@gmail.com>
2025-04-08 19:45:59 -0400

    Updated Lab

--- server.js
+++ server.js
@@ -1,54 +1,48 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const path = require('path'); //"path" is how we will get to the specific folder
 
+//Middleware
 
 ///////////////setup static folder -middleware
-// app.use(express.static(path.join(__dirname, 'public')));
-///////////////////////////////////////////////////
+app.use(express.static("./public"));
+app.set("view engine", "ejs")
 
-
+app.use(express.urlencoded({ extended: true }));
 ///////////////////////////
+
+
 //Basic set up of request/response. this time we are sending the request/response to the html folders we created
 //////////////////////////
 
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// })
+  app.get('/', (req, res) => {
+    res.render('index', {title: "index", content:"This is the index page"});
+})
 
-// app.get('/about', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
-// })
+app.get('/about', (req, res) => {
+    res.render('about', {title: "about", content:"This is the about page"});
+})
+
+app.get("/download", (req, res) =>{
+    res.render('download', {title: "Download", content: "Click below to download the image"})
+})
+
+app.get("/download-image", (req, res) =>{
+    res.download( __dirname + '/public/Ginger.jpeg')
+})
+
+app.get("/greet/:name", (req, res) => {
+    res.send(`Hello, ${req.params.name}!`);
+  });
+  
+  app.post('/submit', (req, res) => {
+    const name = req.body.name; // Gets the name from the form
+    console.log("User submitted name:", name);
+    res.send(`Thank you, ${name}! Your form was submitted.`);
+  });
+  
 
 ///////////////////////////////////////////////////
-
-
-let posts=[
-    {id: 1, title: 'Post One'},
-    {id: 2, title: 'Post Two'},
-    {id: 3, title: 'Post Three'},
-    
-];
-
-//Get all post
-app.get('/api/posts', (req, res) =>{
-    res.json(posts); //serve the post through this endpoint
-
-});
-
-//Get a single post
-app.get('/api/posts/:id', (req, res) =>{
-    const id= parseInt(req.params.id);
-    res.json(posts.filter((post) => post.id === id)); //serve the post through this endpoint
-
-});
-
-
-
-
- 
- 
 
 
 
